496183e4ffb541d9e473c2741e58623c762873a0
marotipawar <pawarmw@gmail.com>
2023-07-15 10:51:41 +0530

    Initial project structure

---
+++ src/main/java/com/maroti/BookMyShow.java
@@ -0,0 +1,13 @@
+package com.maroti;
+
+/**
+ * Hello world!
+ *
+ */
+public class BookMyShow
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/maroti/BookMyShowTest.java
@@ -0,0 +1,35 @@
+package com.maroti;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BookMyShowTest {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public BookMyShowTest(String testName )
+    {
+
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+     return null;
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+
+    }
+}
